d0ad9793bcf922f494974b80c4055d5f4d49fd5d
salih <muhammetsalihaslan@gmail.com>
2023-01-24 15:03:07 +0300

    pages and redux

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,38 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
